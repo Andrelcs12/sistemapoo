b2cf6afd2ec35cf9218aab9c7f59227390b7257e
Jotaerri17 <joaorafaelsantos17@gmail.com>
2025-11-06 20:31:20 -0300

    Fix User class attribute naming and enhance to_dict method to include reservations

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,20 +1,33 @@
 class User:
-    def __init__(self, plano, nome, email, CPF,idade ):
-        self.CPF = CPF
+    def __init__(self, plano, nome, email, cpf,idade ):
+        self.cpf = cpf
         self.nome = nome
         self.email = email
         self.plano = plano
         self.idade = idade
+        self.reservas = [] # reserva do usuario
+
+    def adicionar_reserva(self, reserva):
+        self.reservas.append(reserva)
 
     def to_dict(self):
-        return {"Nome": self.nome, "idade": self.idade, "CPF:": self.CPF, "Email:": self.email, "Plano:": self.plano }
+        return {"Nome": self.nome,
+                "idade": self.idade,
+                "CPF:": self.cpf,
+                "Email:": self.email, 
+                "Plano:": self.plano,
+                "Reservas:": [r.to_dict() for r in self.reservas]} # cria uma lista convertendo cada reserva do usuário usando o método to_dict()
     
 class Reserva:
-    def __init__(self, atividade, data, horario, status):
+    def __init__(self, atividade, data, horario, status="Reservado"):
         self.atividade = atividade
         self.data = data
         self.horario = horario
         self.status = status
 
     def to_reserv(self):
-        return {"Atividade:": self.atividade,"Data:": self.data, "Horario:": self.horario, "Status:": self.status}
+        return {"Atividade:": self.atividade,
+                "Data:": self.data,
+                "Horario:": self.horario,
+                "Status:": self.status}
+    
