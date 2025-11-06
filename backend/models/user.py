8c6e6d338f3281a0f33fa405a041a1c2acd64588
Jotaerri17 <joaorafaelsantos17@gmail.com>
2025-11-06 16:42:45 -0300

    Refactor User class to include additional attributes and update to_dict method

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,8 +1,20 @@
 class User:
-    def __init__(self, user_id, nome, email):
-        self.id = user_id
+    def __init__(self, plano, nome, email, CPF,idade ):
+        self.CPF = CPF
         self.nome = nome
         self.email = email
+        self.plano = plano
+        self.idade = idade
 
     def to_dict(self):
-        return {"id": self.id, "nome": self.nome, "email": self.email}
+        return {"Nome": self.nome, "idade": self.idade, "CPF:": self.CPF, "Email:": self.email, "Plano:": self.plano }
+    
+class Reserva:
+    def __init__(self, atividade, data, horario, status):
+        self.atividade = atividade
+        self.data = data
+        self.horario = horario
+        self.status = status
+
+    def to_reserv(self):
+        return {"Atividade:": self.atividade,"Data:": self.data, "Horario:": self.horario, "Status:": self.status}
